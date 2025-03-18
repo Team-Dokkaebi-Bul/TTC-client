7bee4a6c279b248226c93ed79cfda76d6867c42c
juhyeon <griiim134@gmail.com>
2025-03-18 18:33:05 +0900

    feat : add AWeapon class

---
+++ Assets/@Scripts/AWeapon.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public enum EWeaponType
+{
+    Main, Sub
+};
+
+public enum EWeaponProp
+{
+};
+
+public class AWeapon
+{
+    private int _id;
+    private EWeaponType _type;
+    private EWeaponProp _prop;
+
+    public void InitiateWeapon(in int id, in EWeaponType type, in EWeaponProp prop)
+    {
+        _id = id;
+        _type = type;
+        _prop = prop;
+    }
+
+    public int GetWeaponID()
+    {
+        return _id;
+    }
+
+    public EWeaponType GetWeaponType()
+    {
+        return _type;
+    }
+
+    public EWeaponProp GetWeaponProp()
+    {
+        return _prop;
+    }
+}
