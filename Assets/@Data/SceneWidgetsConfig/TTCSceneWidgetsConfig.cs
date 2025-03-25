c008b0bea55f1be792fdf72d7dc8227f9c2aeb39
이정훈 <eu4ng97@gmail.com>
2025-03-25 13:42:00 +0900

    Feat: OutgameFramework 샘플 설정 제거 및 프로젝트 전용 설정 추가 (#63)
    * Feat: TTCSceneType 열거형 작성
    
    * Feat: TTCSceneWidgetsConfig 스크립터블 오브젝트 작성
    
    * Chore: TTCSceneWidgetsConfig 스크립터블 오브젝트 생성 및 설정
    
    * Chore: 폴더 구조 변경

---
+++ Assets/@Data/SceneWidgetsConfig/TTCSceneWidgetsConfig.cs
@@ -0,0 +1,11 @@
+using Eu4ng.Framework.OutGame;
+using UnityEngine;
+
+/// <summary>
+/// TTC 프로젝트 전용 SceneWidgetsConfig
+/// </summary>
+[CreateAssetMenu(fileName = "TTCSceneWidgetsConfig", menuName = "Scriptable Objects/OutGameFramework/TTCSceneWidgetsConfig")]
+public class TTCSceneWidgetsConfig : SceneWidgetsConfig<TTCSceneType>
+{
+
+}
---
+++ Assets/@Scripts/Types/TTCSceneType.cs
@@ -0,0 +1,9 @@
+/// <summary>
+/// TTC 프로젝트에서 사용할 씬 열거형으로 BuildIndex와 동일한 값으로 설정되어야 합니다. 
+/// </summary>
+public enum TTCSceneType
+{
+    MainMenu = 0,
+    Lobby = 1,
+    TestStage = 2 // 임시
+}
