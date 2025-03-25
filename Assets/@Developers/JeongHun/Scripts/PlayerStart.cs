3384950a5a5eca6cbae22757b0129adb033116d7
이정훈 <eu4ng97@gmail.com>
2025-03-25 11:11:22 +0900

    Feat: PlayerStart 클래스 작성

---
+++ Assets/@Developers/JeongHun/Scripts/PlayerStart.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Eu4ng.Framework.Platformer
+{
+    /// <summary>
+    /// 플레이어 스폰이 가능한 위치
+    /// </summary>
+    public class PlayerStart : MonoBehaviour
+    {
+        
+    }
+}
