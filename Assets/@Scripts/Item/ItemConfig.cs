9965485a4f4f72081425e1b327d46b22da32c23f
이정훈 <eu4ng97@gmail.com>
2025-03-31 20:38:58 +0900

    Feat: ItemConfig 제네릭 타입 클래스 작성 및 InventoryItemConfig 클래스 작성

--- Assets/@Scripts/Item/ItemConfig.cs
+++ Assets/@Scripts/Item/ItemConfig.cs
@@ -4,3 +4,10 @@ public abstract class ItemConfig : ScriptableObject
 {
     
 }
+
+public abstract class ItemConfig<T> : ItemConfig where T : struct
+{
+    [SerializeField] T m_Data;
+    
+    public T Data => m_Data;
+}
