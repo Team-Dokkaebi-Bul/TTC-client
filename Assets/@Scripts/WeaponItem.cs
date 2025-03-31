1bfbb8c91827a54f7643b4069d8d00cad467e5e8
이정훈 <eu4ng97@gmail.com>
2025-03-31 20:56:21 +0900

    Fix: 불필요한 네임스페이스 제거

--- Assets/@Scripts/WeaponItem.cs
+++ Assets/@Scripts/WeaponItem.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections.Generic;
-using Weapon;
 
 public class WeaponItem : MonoBehaviour
 {
--- Assets/@Scripts/WeaponMasteryComponent.cs
+++ Assets/@Scripts/WeaponMasteryComponent.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using System.Collections.Generic;
-using Weapon;
 
 public class WeaponMasteryComponent : MonoBehaviour
 {
