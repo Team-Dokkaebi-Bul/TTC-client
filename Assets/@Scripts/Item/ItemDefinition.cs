3e1114ee122c79ac9c02288425bd669b04be9130
이정훈 <eu4ng97@gmail.com>
2025-03-31 20:32:03 +0900

    Feat: ItemConfig 클래스 작성

--- Assets/@Scripts/Item/ItemDefinition.cs
+++ Assets/@Scripts/Item/ItemDefinition.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ItemDefinition", menuName = "Scriptable Objects/ItemDefinition")]
 public class ItemDefinition : ScriptableObject
 {
     public IStat GetStat() => null;
+    
+    [SerializeField] List<ItemConfig> m_ItemConfigs = new List<ItemConfig>();
+    
+    protected List<ItemConfig> ItemConfigs => m_ItemConfigs;
+
+    public T GetItemConfig<T>() where T : ItemConfig
+    {
+        foreach (var itemConfig in ItemConfigs)
+        {
+            // 유효성 검사
+            if (itemConfig == null) continue;
+            
+            if(itemConfig.GetType().IsSubclassOf(typeof(T))) return (T)itemConfig;
+        }
+
+        return null;
+    }
 }
