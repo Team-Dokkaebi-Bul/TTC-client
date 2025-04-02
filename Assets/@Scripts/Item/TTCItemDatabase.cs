6f89d6109aab41533475ee285c4bb830f5017015
이정훈 <eu4ng97@gmail.com>
2025-04-02 17:17:04 +0900

    Feat: TTCClientDataTableRow 클래스를 파일로 분리

--- Assets/@Scripts/Item/TTCItemDatabase.cs
+++ Assets/@Scripts/Item/TTCItemDatabase.cs
@@ -3,14 +3,6 @@ using Eu4ng.System.Item;
 using UnityEngine;
 
 /// <summary>
-/// TTC 프로젝트 전용 데이터 테이블 데이터 클래스
-/// </summary>
-public class TTCItemDataTableRow : ItemDataTableRow
-{
-    
-}
-
-/// <summary>
 /// TTC 프로젝트 전용 아이템 데이터베이스
 /// </summary>
 [CreateAssetMenu(fileName = "TTCItemDatabase", menuName = "Scriptable Objects/Item/Database")]
