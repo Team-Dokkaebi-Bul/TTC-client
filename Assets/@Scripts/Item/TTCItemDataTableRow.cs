b5a7617b8a53fbaa732ac8069c27dd0124be0d04
이정훈 <eu4ng97@gmail.com>
2025-04-04 13:07:38 +0900

    Feat: 아이템 시스템 개편 (#83)
    * Fix: 아이템 관련 레거시 스크립트 제거
    
    * Chore: GameFramework 패키지 v0.1.0 설치
    
    * Chore: GameFramework 패키지 샘플 에셋 임포트
    
    * Feat: TTCItemDatabase 클래스 작성
    
    * Chore: 폴더 구조 변경
    
    * Chore: TTC 프로젝트 전용 샘플 아이템 에셋 생성
    
    * Chore: ItemSystemSettings에 TTCItemDatabase 등록
    
    * Feat: TTCClientDataTableRow 클래스를 파일로 분리

---
+++ Assets/@Scripts/Item/TTCItemDataTableRow.cs
@@ -0,0 +1,19 @@
+using Eu4ng.System.Item;
+
+/// <summary>
+/// TTC 프로젝트 전용 데이터 테이블 데이터 클래스
+/// </summary>
+public class TTCItemDataTableRow : ItemDataTableRow
+{
+    // InventoryItemConfig
+    
+    public int MaxStack;
+    
+    // EquipmentItemConfig
+    
+    // TODO
+    
+    // StatItemConfig
+    
+    // TODO
+}
---
+++ Assets/@Scripts/Item/TTCItemDatabase.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using Eu4ng.System.Item;
+using UnityEngine;
+
+/// <summary>
+/// TTC 프로젝트 전용 아이템 데이터베이스
+/// </summary>
+[CreateAssetMenu(fileName = "TTCItemDatabase", menuName = "Scriptable Objects/Item/Database")]
+public class TTCItemDatabase : ItemDatabase<TTCItemDataTableRow>
+{
+#if UNITY_EDITOR
+    [ContextMenu("HardUpdate")]
+    protected override void HardUpdate() => base.HardUpdate();
+
+    [ContextMenu("SoftUpdate")]
+    protected override void SoftUpdate() => base.SoftUpdate();
+
+    protected override void OnItemDefinitionCreated(int id, TTCItemDataTableRow dataTableRow, ItemDefinition itemDefinition,
+        List<ItemConfig> itemConfigs)
+    {
+        // Assets/Samples/GameFramework/VERSION/InventorySystem Sample/Scripts/SampleItemDatabase.cs 참고
+        
+        RegisterInventoryItemConfig(id, dataTableRow, itemDefinition, itemConfigs);
+        RegisterEquipmentItemConfig(id, dataTableRow, itemDefinition, itemConfigs);
+        RegisterStatItemConfig(id, dataTableRow, itemDefinition, itemConfigs);
+    }
+
+    protected void RegisterInventoryItemConfig(int id, TTCItemDataTableRow dataTableRow, ItemDefinition itemDefinition,
+        List<ItemConfig> itemConfigs)
+    {
+        // TODO
+    }
+    
+    protected void RegisterEquipmentItemConfig(int id, TTCItemDataTableRow dataTableRow, ItemDefinition itemDefinition,
+        List<ItemConfig> itemConfigs)
+    {
+        // TODO
+    }
+    
+    protected void RegisterStatItemConfig(int id, TTCItemDataTableRow dataTableRow, ItemDefinition itemDefinition,
+        List<ItemConfig> itemConfigs)
+    {
+        // TODO
+    }
+#endif
+}
